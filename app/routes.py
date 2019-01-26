19b4ccdf88bfe16664f9958e6274ed0fa3093e62
bush-man92 <42248918+bush-man92@users.noreply.github.com>
2019-01-26 18:01:33 +0100

    obavezno

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,10 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
-from app.forms import RegistrationForm
+from app.forms import RegistationForm
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-	form = RegistrationForm()
+	form = RegistationForm()
+	if form.validate_on_submit():
+	    return redirect('/register')
 	return render_template('register.html', title='Register', form=form)
