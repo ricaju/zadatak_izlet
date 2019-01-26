cbc187251cd8ae78cec2ca6ae1efed5c6f26cf41
sergioraskovic <raskovic.sergio7@gmail.com>
2019-01-26 21:11:06 +0100

    jadsasjfb

--- app/models.py
+++ app/models.py
@@ -1,10 +1,29 @@
+from datetime import datetime
 from app import db
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
+    username = db.Column(db.String(64),index=True,unique=True)
+    email = db.Column(db.String(120),index=True,unique=True)
     password_hash = db.Column(db.String(128))
 
-    def __repr__(self):
-        return '<User {}>'.format(self.username)    
+    
+
+class Trip(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    location = db.Column(db.String(120),index=True)
+    about = db.Column(db.String(1000),index=True)
+    date = db.Column(db.DateTime,index=True)
+    min_people = db.Column(db.String(120),index=True)
+    max_people = db.Column(db.String(120),index=True)
+    cost = db.Column(db.Integer,index=True)
+    user_id = db.Column(db.Integer,db.ForeignKey('user.id'))
+    creator_id = db.Column(db.Integer,db.ForeignKey('user.id'))
+    trip_rating = db.Column(db.Integer)
+
+class User_rating(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_rating = db.Column(db.Integer)
+    user_id = db.Column(db.Integer,db.ForeignKey('user.id'))
+
