d8ca9ab74268c309409750c53d803c48c8bb82a3
bush-man92 <42248918+bush-man92@users.noreply.github.com>
2019-01-26 17:51:30 +0100

    12345

--- app/forms.py
+++ app/forms.py
@@ -5,6 +5,6 @@ from wtforms.validators import DataRequired, EqualTo, Email
 class RegistationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
-    password2 = PasswordField('Password', validators=[DataRequired(), EqualTo(password)])
+    password2 = PasswordField('Password', validators=[DataRequired(), EqualTo('password')])
     email = StringField('Email', validators=[DataRequired(), Email()])
     submit = SubmitField('Register')
